6cd7b1a46333b327df1a41d03dc8e272aa339a3d
Kevin Lee <kevin.code@kevinlee.io>
2023-01-14 23:41:06 +1100

    Add: HttpClient

--- examples/effectie2-examples-cats-effect3/src/main/scala-2/example/service/Jokes.scala
+++ examples/effectie2-examples-cats-effect3/src/main/scala-2/example/service/Jokes.scala
@@ -7,16 +7,17 @@ import effectie.core._
 import effectie.syntax.all._
 import eu.timepit.refined.types.string.NonEmptyString
 import example.config.AppConfig.JokesConfig
+import example.http4s.HttpClient
 import io.circe.Codec
 import io.circe.generic.semiauto._
 import io.estatico.newtype.macros.newtype
 import loggerf.core._
 import loggerf.syntax.all._
 import org.http4s.Method._
-import org.http4s.Uri
+import org.http4s.{MediaType, Uri}
 import org.http4s.circe.CirceEntityCodec._
-import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
+import org.http4s.headers.Accept
 
 import scala.util.control.NoStackTrace
 
@@ -49,20 +50,40 @@ object Jokes {
     def fromConfig(jokesConfig: JokesConfig): JokesUri = JokesUri(jokesConfig.uri.value)
   }
 
-  def apply[F[*]: Fx: Concurrent: Log](uri: JokesUri)(C: Client[F])(implicit dsl: Http4sClientDsl[F]): Jokes[F] =
+  def apply[F[*]: Fx: Concurrent: Log](uri: JokesUri)(C: HttpClient[F])(implicit dsl: Http4sClientDsl[F]): Jokes[F] =
     new Jokes[F] {
       import dsl._
       import org.http4s.syntax.all._
 
       def get: F[Either[Jokes.JokeError, Jokes.Joke]] =
-        C.expect[Joke](GET(uri.value))
-          .catchNonFatal { err =>
-            JokeError(NonEmptyString.unsafeFrom(s"Error when getting a joke from ${uri.value.show}"), err.some)
-          }
+        C.sendAndHandle(GET(uri.value, Accept(MediaType.application.json))) {
+          case HttpClient.HttpResponse.Successful(res) =>
+            res
+              .as[Joke]
+              .catchNonFatal {
+                case err =>
+                  JokeError(NonEmptyString.unsafeFrom(s"Error when getting a joke from ${uri.value.show}"), err.some)
+              }
+          case HttpClient.HttpResponse.Failed(res) =>
+            HttpClient
+              .bodyToUtf8String(res)
+              .log(
+                error(s"Failed response: ${res.status.show}, No response body"),
+                message => error(s"Failed response: ${res.status.show}, body: $message")
+              )
+              .map { message =>
+                JokeError(
+                  NonEmptyString.unsafeFrom(
+                    s"Error when getting a joke from ${uri.value.show}. Message: ${message.show}"
+                  ),
+                  none
+                ).asLeft[Jokes.Joke]
+              }
+        }
 
       def testTimeout: F[String] =
         ">>> Start test-timeout".logS(info) *>
-          C.expect[String](GET(uri"http://0.0.0.0:8080/take-seconds/30"))
+          C.send[String](GET(uri"http://0.0.0.0:8080/take-seconds/30"))
 
     }
 }
